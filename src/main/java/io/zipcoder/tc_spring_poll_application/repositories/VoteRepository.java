151ecf64358263da2c217f7a8c379091b0d03bf7
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-04-20 12:28:08 -0400

    6.1 done

--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
@@ -9,5 +9,5 @@ public interface VoteRepository extends CrudRepository<Vote, Long> {
             "FROM Option o, Vote v " +
             "WHERE o.POLL_ID = ?1 " +
             "AND v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
-    public Iterable<Vote> findVotesByPoll(Long pollId);
+    Iterable<Vote> findVotesByPoll(Long pollId);
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,59 @@
+-- first poll
+insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (1, 'Red', 1), (2, 'Blue', 1), (3, 'Green', 1);
+
+-- second poll
+insert into poll (poll_id, question) values (2, 'What is your favorite food?');
+insert into option (option_id, option_value, poll_id) values (1, 'Rice', 2), (2, 'Bread', 2), (3, 'Pizza', 2);
+
+-- third poll
+insert into poll (poll_id, question) values (3, 'What is your favorite movie?');
+insert into option (option_id, option_value, poll_id) values (1, 'Momento', 3), (2, 'Avengers', 3), (3, 'Dark Knight', 3);
+
+-- fourth poll
+insert into poll (poll_id, question) values (4, 'What is your favorite anime?');
+insert into option (option_id, option_value, poll_id) values (1, 'One Piece', 4), (2, 'DBZ', 4), (3, 'Naruto', 4);
+
+-- fifth poll
+insert into poll (poll_id, question) values (5, 'What is your favorite language?');
+insert into option (option_id, option_value, poll_id) values (1, 'Java', 5), (2, 'Python', 5), (3, 'C', 5);
+
+-- sixth poll
+insert into poll (poll_id, question) values (6, 'What is your favorite game?');
+insert into option (option_id, option_value, poll_id) values (1, 'League', 6), (2, 'Dota', 6), (3, 'Mario', 6);
+
+-- seventh poll
+insert into poll (poll_id, question) values (7, 'What is your favorite music?');
+insert into option (option_id, option_value, poll_id) values (1, 'EDM', 7), (2, 'Hip Hop', 7), (3, 'Jazz', 7);
+
+-- eighth poll
+insert into poll (poll_id, question) values (8, 'What is your favorite chips?');
+insert into option (option_id, option_value, poll_id) values (1, 'Lays', 8), (2, 'Herrs', 8), (3, 'Pringles', 8);
+
+-- ninth poll
+insert into poll (poll_id, question) values (9, 'What is your favorite soda?');
+insert into option (option_id, option_value, poll_id) values (1, 'pepsi', 9), (2, 'sprite', 9), (3, 'fanta', 9);
+
+-- tenth poll
+insert into poll (poll_id, question) values (10, 'What is your favorite OS?');
+insert into option (option_id, option_value, poll_id) values (1, 'Windows', 10), (2, 'Mac', 10), (3, 'Linux', 10);
+
+-- eleventh poll
+insert into poll (poll_id, question) values (11, 'What is your favorite phone?');
+insert into option (option_id, option_value, poll_id) values (1, 'apple', 11), (2, 'samsung', 11), (3, 'nokia', 11);
+
+-- twelfth poll
+insert into poll (poll_id, question) values (12, 'What is your favorite fighter?');
+insert into option (option_id, option_value, poll_id) values (1, 'lomachenko', 12), (2, 'inoue', 12), (3, 'pacqiao', 12);
+
+-- thirteenth poll
+insert into poll (poll_id, question) values (13, 'What is your favorite car?');
+insert into option (option_id, option_value, poll_id) values (1, 'Tesla', 13), (2, 'Hyundai', 13), (3, 'Honda', 13);
+
+-- fourteenth poll
+insert into poll (poll_id, question) values (14, 'What is your favorite drink?');
+insert into option (option_id, option_value, poll_id) values (1, 'rum', 14), (2, 'vodka', 14), (3, 'whisky', 14);
+
+-- fifteenth poll
+insert into poll (poll_id, question) values (15, 'What is your favorite snack?');
+insert into option (option_id, option_value, poll_id) values (1, 'chips', 15), (2, 'cookies', 15), (3, 'candy', 15);
