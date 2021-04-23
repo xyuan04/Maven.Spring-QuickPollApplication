3d815cca699174bac3fd559cdb5e345b77415831
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-04-22 20:12:42 -0400

    fixed sql file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,56 +4,56 @@ insert into option (option_id, option_value, poll_id) values (1, 'Red', 1), (2, 
 
 -- second poll
 insert into poll (poll_id, question) values (2, 'What is your favorite food?');
-insert into option (option_id, option_value, poll_id) values (1, 'Rice', 2), (2, 'Bread', 2), (3, 'Pizza', 2);
+insert into option (option_id, option_value, poll_id) values (4, 'Rice', 2), (5, 'Bread', 2), (6, 'Pizza', 2);
 
 -- third poll
 insert into poll (poll_id, question) values (3, 'What is your favorite movie?');
-insert into option (option_id, option_value, poll_id) values (1, 'Momento', 3), (2, 'Avengers', 3), (3, 'Dark Knight', 3);
+insert into option (option_id, option_value, poll_id) values (7, 'Momento', 3), (8, 'Avengers', 3), (9, 'Dark Knight', 3);
 
 -- fourth poll
 insert into poll (poll_id, question) values (4, 'What is your favorite anime?');
-insert into option (option_id, option_value, poll_id) values (1, 'One Piece', 4), (2, 'DBZ', 4), (3, 'Naruto', 4);
+insert into option (option_id, option_value, poll_id) values (10, 'One Piece', 4), (11, 'DBZ', 4), (12, 'Naruto', 4);
 
 -- fifth poll
 insert into poll (poll_id, question) values (5, 'What is your favorite language?');
-insert into option (option_id, option_value, poll_id) values (1, 'Java', 5), (2, 'Python', 5), (3, 'C', 5);
+insert into option (option_id, option_value, poll_id) values (13, 'Java', 5), (14, 'Python', 5), (15, 'C', 5);
 
 -- sixth poll
 insert into poll (poll_id, question) values (6, 'What is your favorite game?');
-insert into option (option_id, option_value, poll_id) values (1, 'League', 6), (2, 'Dota', 6), (3, 'Mario', 6);
+insert into option (option_id, option_value, poll_id) values (16, 'League', 6), (17, 'Dota', 6), (18, 'Mario', 6);
 
 -- seventh poll
 insert into poll (poll_id, question) values (7, 'What is your favorite music?');
-insert into option (option_id, option_value, poll_id) values (1, 'EDM', 7), (2, 'Hip Hop', 7), (3, 'Jazz', 7);
+insert into option (option_id, option_value, poll_id) values (19, 'EDM', 7), (20, 'Hip Hop', 7), (21, 'Jazz', 7);
 
 -- eighth poll
 insert into poll (poll_id, question) values (8, 'What is your favorite chips?');
-insert into option (option_id, option_value, poll_id) values (1, 'Lays', 8), (2, 'Herrs', 8), (3, 'Pringles', 8);
+insert into option (option_id, option_value, poll_id) values (22, 'Lays', 8), (23, 'Herrs', 8), (24, 'Pringles', 8);
 
 -- ninth poll
 insert into poll (poll_id, question) values (9, 'What is your favorite soda?');
-insert into option (option_id, option_value, poll_id) values (1, 'pepsi', 9), (2, 'sprite', 9), (3, 'fanta', 9);
+insert into option (option_id, option_value, poll_id) values (25, 'pepsi', 9), (26, 'sprite', 9), (27, 'fanta', 9);
 
 -- tenth poll
 insert into poll (poll_id, question) values (10, 'What is your favorite OS?');
-insert into option (option_id, option_value, poll_id) values (1, 'Windows', 10), (2, 'Mac', 10), (3, 'Linux', 10);
+insert into option (option_id, option_value, poll_id) values (28, 'Windows', 10), (29, 'Mac', 10), (30, 'Linux', 10);
 
 -- eleventh poll
 insert into poll (poll_id, question) values (11, 'What is your favorite phone?');
-insert into option (option_id, option_value, poll_id) values (1, 'apple', 11), (2, 'samsung', 11), (3, 'nokia', 11);
+insert into option (option_id, option_value, poll_id) values (31, 'apple', 11), (32, 'samsung', 11), (33, 'nokia', 11);
 
 -- twelfth poll
 insert into poll (poll_id, question) values (12, 'What is your favorite fighter?');
-insert into option (option_id, option_value, poll_id) values (1, 'lomachenko', 12), (2, 'inoue', 12), (3, 'pacqiao', 12);
+insert into option (option_id, option_value, poll_id) values (34, 'lomachenko', 12), (35, 'inoue', 12), (36, 'pacqiao', 12);
 
 -- thirteenth poll
 insert into poll (poll_id, question) values (13, 'What is your favorite car?');
-insert into option (option_id, option_value, poll_id) values (1, 'Tesla', 13), (2, 'Hyundai', 13), (3, 'Honda', 13);
+insert into option (option_id, option_value, poll_id) values (37, 'Tesla', 13), (38, 'Hyundai', 13), (39, 'Honda', 13);
 
 -- fourteenth poll
 insert into poll (poll_id, question) values (14, 'What is your favorite drink?');
-insert into option (option_id, option_value, poll_id) values (1, 'rum', 14), (2, 'vodka', 14), (3, 'whisky', 14);
+insert into option (option_id, option_value, poll_id) values (40, 'rum', 14), (41, 'vodka', 14), (42, 'whisky', 14);
 
 -- fifteenth poll
 insert into poll (poll_id, question) values (15, 'What is your favorite snack?');
-insert into option (option_id, option_value, poll_id) values (1, 'chips', 15), (2, 'cookies', 15), (3, 'candy', 15);
+insert into option (option_id, option_value, poll_id) values (43, 'chips', 15), (44, 'cookies', 15), (45, 'candy', 15);
